db98589a0290d53d776a40b7c7151c49aa0eb34d
patrickianwilson <patrick.ian.wilson@gmail.com>
2016-07-16 20:41:44 -0700

    added a build.sh file to suggest how to actually run this build.

---
+++ Client/NativeDesktop/NativeDesktopGraphics/build.sh
@@ -0,0 +1,6 @@
+# this is a file that captures the essence of how to build.
+
+export CMAKE_PREFIX_PATH=/Users/pwilson/Qt/5.7/clang_64/lib/cmake/Qt5Core:/Users/pwilson/Qt/5.7/clang_64/lib/cmake/Qt5Widgets
+
+cmake .
+make
